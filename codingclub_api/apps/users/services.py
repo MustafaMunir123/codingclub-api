6d8f52ebe604a7e23d1bb8a29325c10818cac92d
MustafaMunir123 <mustafamunir10@gmail.com>
2023-03-31 06:41:50 +0500

    feat: implement firebase logic

---
+++ codingclub_api/apps/users/services.py
@@ -0,0 +1,28 @@
+import os
+import uuid
+
+import pyrebase
+import ast
+from dotenv import load_dotenv
+
+# Loading .env file
+load_dotenv('config/.env')
+
+# getting config str-dict and convert it into dict type using ast.literal_eval
+config = ast.literal_eval(os.getenv("REALTIME_DB_CONFIG"))
+email = os.getenv("FB_EMAIL")
+password = os.getenv("FB_PASSWORD")
+
+# Setting up Firebase storage
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+user = auth.sign_in_with_email_and_password(email=email, password=password)
+storage = firebase.storage()
+
+
+def store_image_get_url(image_file, path: str):
+    image_file.name = f"{uuid.uuid4()} _ {image_file.name}"
+    storage.child("coding_club-api/media/" + path + image_file.name).put(image_file, token=user["idToken"])
+    image_url = storage.child("coding_club-api/media/" + path + image_file.name).get_url(token=None)
+    print(image_url)
+    return image_url
