a057a627f098ba2e9b4e3e37ce2b025c9ad9d300
MustafaMunir123 <mustafamunir10@gmail.com>
2023-04-04 18:07:20 +0500

    feat: add api to get list of rejected clubs

--- codingclub_api/apps/users/api/v1/urls.py
+++ codingclub_api/apps/users/api/v1/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("v1/user/", UserApiView.as_view()),
     path("v1/user/<uuid:pk>", UserApiView.as_view()),
     path("v1/club_status/<uuid:pk>", AdminApiView.as_view()),
-    path("v1/get_all_clubs/", AdminApiView.as_view()),
+    path("v1/get_club_requests/", AdminApiView.as_view()),
     path("v1/get_admin_users/", AdminApiView.as_view()),
+    path("v1/get_rejected_clubs/", AdminApiView.as_view()),
 ]
