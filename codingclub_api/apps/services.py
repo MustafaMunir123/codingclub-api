0fa710dace81331db2af2afe08135a29e66c6800
MustafaMunir123 <mustafamunir10@gmail.com>
2023-04-12 09:20:32 +0500

    chore: move convert_to_id to apps/services.py

--- codingclub_api/apps/services.py
+++ codingclub_api/apps/services.py
@@ -4,6 +4,7 @@ import uuid
 import pyrebase
 import ast
 from dotenv import load_dotenv
+from typing import Dict
 
 # Loading .env file
 load_dotenv('config/.env')
@@ -33,6 +34,24 @@ def store_image_get_url(image_file, path: str):
 
 
 def delete_image_from_url(url_path):
-    # url=storage.child(url_path).get_url(None)
+    firebase = pyrebase.initialize_app(config)
+    auth = firebase.auth()
+    user = auth.sign_in_with_email_and_password(email=email, password=password)
+    storage = firebase.storage()    # url=storage.child(url_path).get_url(None)
     storage.delete(url_path, token=user["idToken"])
+
+
+def convert_to_id(dictionary_list: Dict, ManyToManyModel):
+    objects_id = []
+    for key, value in dictionary_list.items():
+        objects_id = []
+        print(key, value)
+        dictionary_list = value.split(',') #ast.literal_eval(value)
+        print(dictionary_list)
+        for obj in dictionary_list:
+            kwargs = {key: obj}
+            print(kwargs)
+            obj = ManyToManyModel.objects.get(**kwargs)
+            objects_id.append(obj.id)
+    return objects_id
 
